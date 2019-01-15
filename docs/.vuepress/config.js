3a341b1250be3afd96e6e16b1ffde71204ad0d1d
Zak Linder <zak@birchbox.com>
2019-01-14 22:55:31 -0500

    add sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,21 @@
 module.exports = {
   title: 'Bernard Docs',
-  description: 'Evaluating VuePress for Birchbox'
+  description: 'Evaluating VuePress for Birchbox',
+  themeConfig: {
+    sidebar: [
+      '/',
+      {
+        title: 'Guides',
+        children: [
+          '/guides/addresses',
+          '/guides/event-logging/',
+          '/guides/routing/',
+        ],
+      },
+      '/components/',
+      '/enhancers/',
+      '/middleware/',
+      '/utils/',
+    ],
+  },
 }
